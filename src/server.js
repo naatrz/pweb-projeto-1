8df48bae0971d5e48dd350e5a059d5bf0652b8da
AnaQuezia06 <quezia.soares@aluno.ifce.edu.com.br>
2026-03-21 10:46:49 -0300

    teste server.js

---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// 🔥 dados (simulando banco)
+let registers = [];
+
+// LOGIN
+app.post('/login', (req, res) => {
+  const { email, senha } = req.body;
+
+  if (email === "admin@email.com" && senha === "admin123") {
+    return res.json({ token: "Security-token" });
+  }
+
+  res.status(401).json({ erro: "Login inválido" });
+});
+
+// MIDDLEWARE
+function auth(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token === "Security-token") {
+    next();
+  } else {
+    res.status(403).json({ erro: "Acesso negado" });
+  }
+}
+
+// registros (protegido)
+app.get('/registers', auth, (req, res) => {
+  res.json(registers);
+});
+
+// cadastro
+app.post('/registers', (req, res) => {
+  const novo = req.body;
+  registers.push(novo);
+  res.json(novo);
+});
+
+app.get('/', (req, res) => {
+  res.send('API funcionando 🎮');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
